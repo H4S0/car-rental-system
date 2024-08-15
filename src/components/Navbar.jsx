df4cdfc3a7c8fcd3bbf90cbfcb3b5dd2024a2111
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-15 15:27:47 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,27 @@
-import React from "react";
 import { Link } from "react-router-dom";
-const Navbar = () => {
+
+function Navbar() {
   return (
-    <div className="flex flex-row">
-      <div>xRENT</div>
-      <div></div>
-      <div></div>
-    </div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cars">Cars</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/signup">Sign Up</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
   );
-};
+}
 
 export default Navbar;
