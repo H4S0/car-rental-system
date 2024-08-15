e12cec1dcb5c6cd32cbb51c152e1a65ffe58a11c
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-15 15:27:30 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./sections/HomePage";
+import Cars from "./sections/Cars";
+import About from "./sections/About";
+import SignUp from "./sections/SignUp";
+import Navbar from "./components/Navbar";
+import Login from "./sections/Login";
+
 
 function App() {
   return (
-    <BrowserRouter>
+    <div>
+      <Navbar />
       <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="cars" element={<Cars />} />
-        <Route path="about" element={<About />} />
-        <Route path="signup" element={<SignUp />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
 
