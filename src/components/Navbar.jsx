3abf826f9f03091fb72d6e9d609b030d55f8690d
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-16 16:09:52 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { supabase } from "../services/supabase";
 
-function Navbar() {
+function Navbar({ isLogged, setIsLogged }) {
+  const [email, setEmail] = useState(null);
+
+  useEffect(() => {
+    async function fetchUser() {
+      if (isLogged) {
+        const res = await supabase.auth.getSession();
+        const user = res.data?.session?.user;
+        if (user) {
+          setEmail(user.email);
+        }
+      } else {
+        setEmail(null);
+      }
+    }
+    fetchUser();
+  }, [isLogged]);
+
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Error signing out:", error.message);
+    } else {
+      setIsLogged(false); // Update the logged-in state
+      setEmail(null); // Clear the email from state
+    }
+  };
+
   return (
     <nav>
       <ul>
@@ -13,13 +42,22 @@ function Navbar() {
         <li>
           <Link to="/about">About</Link>
         </li>
-        <li>
-          <Link to="/signup">Sign Up</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
       </ul>
+      {isLogged ? (
+        <div>
+          <span>{email}</span>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      )}
     </nav>
   );
 }
