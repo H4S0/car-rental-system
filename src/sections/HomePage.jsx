6ce41910163bedb940c4008090f6810bd9cf0d93
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-17 14:09:36 +0200

    Update HomePage.jsx

--- src/sections/HomePage.jsx
+++ src/sections/HomePage.jsx
@@ -1,7 +1,31 @@
-import Navbar from "../components/Navbar";
+import Button from "../components/Button";
+import { Link } from "react-router-dom";
+import { CiLocationOn } from "react-icons/ci";
 
 const HomePage = () => {
-  return <div>test</div>;
+  return (
+    <section className="flex justify-between items-center p-8 mt-[5%]">
+      <div className="flex flex-col gap-3 w-1/2 ">
+        <h2 className="text-4xl md:text-6xl font-bold">
+          The easy way to takeover a lease
+        </h2>
+        <div className="flex flex-row items-center gap-2">
+          <CiLocationOn className="text-5xl md:text-4xl" />
+          <p className="text-2xl">Sarajevo Airport</p>
+        </div>
+        <Link to="/cars">
+          <Button>Rent a car</Button>
+        </Link>
+      </div>
+      <div className="w-1/2 p-6">
+        <img
+          src={"/src/assets/Audi-A6.webp"}
+          alt="audi"
+          className="w-full h-auto rounded-lg shadow-lg"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
