54ab177bc12e9622648b79e4b71eb239c3ff47f8
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-17 14:09:08 +0200

    Add files via upload

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ children }) => {
+  return (
+    <button className="inline-flex items-center justify-center px-4 py-2 font-sans font-semibold tracking-wide text-white bg-blue-500 rounded-lg ">
+      {children}
+    </button>
+  );
+};
+
+export default Button;
