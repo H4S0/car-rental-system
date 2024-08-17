9cec7865b089c07bc47db11ecbc5c542c487ac77
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-17 14:08:11 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,31 +31,73 @@ function Navbar({ isLogged, setIsLogged }) {
   };
 
   return (
-    <nav>
-      <ul>
+    <nav className="flex flex-col md:flex-row justify-between items-center p-4">
+      <ul className="flex flex-row gap-10 text-gray-700">
         <li>
-          <Link to="/">Home</Link>
+          <Link
+            to="/"
+            className="hover:text-sky-500 transition-colors duration-300"
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/cars">Cars</Link>
+          <Link
+            to="/howworks"
+            className="hover:text-blue-500 transition-colors duration-300"
+          >
+            How it works
+          </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link
+            to="/cars"
+            className="hover:text-blue-500 transition-colors duration-300"
+          >
+            Cars
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/whychoose"
+            className="hover:text-blue-500 transition-colors duration-300"
+          >
+            Why choose us
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/contact"
+            className="hover:text-blue-500 transition-colors duration-300"
+          >
+            Contact
+          </Link>
         </li>
       </ul>
       {isLogged ? (
-        <div>
+        <div className="flex flex-row items-center gap-4 text-gray-700">
           <span>{email}</span>
-          <button onClick={handleLogout}>Logout</button>
+          <button
+            onClick={handleLogout}
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-sky-600 transition-colors duration-300"
+          >
+            Logout
+          </button>
         </div>
       ) : (
-        <div>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
+        <div className="flex flex-row gap-5">
+          <Link
+            to="/signup"
+            className="bg-blue-500 text-white px-4 py-2 rounded-xl hover:bg-sky-600 transition-colors duration-300"
+          >
+            Sign Up
+          </Link>
+          <Link
+            to="/login"
+            className="bg-gray-500 text-white px-4 py-2 rounded-xl hover:bg-gray-600 transition-colors duration-300"
+          >
+            Login
+          </Link>
         </div>
       )}
     </nav>
