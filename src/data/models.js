bd3e76595e0a4d9f9fe1748efd58518774e658d6
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-19 09:26:30 +0200

    Add files via upload

---
+++ src/data/models.js
@@ -0,0 +1,35 @@
+import { SiAudi } from "react-icons/si";
+import { SiBmw } from "react-icons/si";
+import { SiVolkswagen } from "react-icons/si";
+import { SiVolvo } from "react-icons/si";
+import { SiSkoda } from "react-icons/si";
+
+const models = [
+  {
+    id: 1,
+    name: "Audi",
+    img: SiAudi,
+  },
+  {
+    id: 2,
+    name: "BMW",
+    img: SiBmw,
+  },
+  {
+    id: 3,
+    name: "WW",
+    img: SiVolkswagen,
+  },
+  {
+    id: 4,
+    name: "Volvo",
+    img: SiVolvo,
+  },
+  {
+    id: 4,
+    name: "Skoda",
+    img: SiSkoda,
+  },
+];
+
+export default models;
