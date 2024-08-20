489c13834abbfbaa3a7649f8c88bb756aa01816f
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-20 17:52:36 +0200

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ children }) => {
   return (
-    <button className="inline-flex items-center justify-center px-4 py-2 font-sans font-semibold tracking-wide text-white bg-blue-500 rounded-lg ">
+    <button className="inline-flex items-center justify-center px-4 py-2 font-sans font-semibold tracking-wide text-white bg-blue-500 rounded-lg hover:bg-sky-600 transition-colors duration-300 ">
       {children}
     </button>
   );
