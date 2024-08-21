be7c28a687c41f7071f16f01420fa0633d4ec7c4
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-21 13:38:45 +0200

    Update Cars.jsx

--- src/sections/Cars.jsx
+++ src/sections/Cars.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import cars from "../data/cars";
+import CarCard from "../components/CarCard";
 
 const Cars = () => {
-  return <div>Cars</div>;
+  return (
+    <section>
+      <h2 className="">Explore our cars menu</h2>
+      <div className="">
+        <div className="">
+          {cars.map((car) => (
+            <CarCard key={car.id} car={car} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Cars;
