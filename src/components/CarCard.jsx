b8b60585765b31467d7edbb72c87cdf6534badba
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-21 20:31:11 +0200

    Update CarCard.jsx

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -1,7 +1,29 @@
-import React from "react";
-
 const CarCard = ({ car }) => {
-  return <div>CarCard</div>;
+  return (
+    <div className="bg-white shadow-md rounded-lg overflow-hidden flex flex-col p-4">
+      <img
+        src={car.picture}
+        alt={car.model}
+        className="w-full h-[150px] object-cover rounded-md mb-4"
+      />
+      <div className="flex flex-col bg-white">
+        <h2 className="bg-white text-xl font-semibold mb-2">{car.model}</h2>
+        <p className="bg-white text-gray-600 mb-1">{car.mark}</p>
+        <p className="bg-white text-gray-600 mb-1">
+          Transmission: {car.transmission}
+        </p>
+
+        <div className="bg-white flex flex-col sm:flex-row sm:item-center justify-between items-start mt-5 gap-3">
+          <p className="bg-blue-500 text-white font-bold rounded-lg px-3 py-2 w-max">
+            ${car.price} /per day
+          </p>
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg px-4 py-2 sm:w-max transition-all duration-300">
+            Rent a car
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CarCard;
