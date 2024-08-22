f736dfa4c58cc87a5c60a66dbf7bf42fcfb44f95
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-22 11:56:56 +0200

    Update Cars.jsx

--- src/sections/Cars.jsx
+++ src/sections/Cars.jsx
@@ -4,13 +4,11 @@ import CarCard from "../components/CarCard";
 const Cars = () => {
   return (
     <section>
-      <h2 className="">Explore our cars menu</h2>
-      <div className="">
-        <div className="">
-          {cars.map((car) => (
-            <CarCard key={car.id} car={car} />
-          ))}
-        </div>
+      <h2 className="text-center text-3xl mb-10">Explore our cars menu</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full px-4 lg:px-0">
+        {cars.map((car) => (
+          <CarCard key={car.model} car={car} />
+        ))}
       </div>
     </section>
   );
