80546527513263227c7061b15528cba816039365
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-25 19:00:33 +0200

    Update Models.jsx

--- src/components/Models.jsx
+++ src/components/Models.jsx
@@ -2,8 +2,8 @@ const Models = ({ model }) => {
   const Icon = model.img;
 
   return (
-    <div className="flex flex-wrap mt-10">
-      <Icon size={80} className="hover:text-blue-500" />
+    <div className="flex flex-wrap mt-10 justify-center sm:justify-start">
+      <Icon className="text-5xl sm:text-7xl hover:text-blue-500 mx-2" />
     </div>
   );
 };
