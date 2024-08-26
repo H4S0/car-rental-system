03727a19c6176850954c67c78713ec4e910ab2c5
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-26 18:17:29 +0200

    Update CarCard.jsx

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -1,3 +1,8 @@
+import { BsFillFuelPumpFill } from "react-icons/bs";
+import { GiGearStickPattern } from "react-icons/gi";
+import { FaCalendarAlt } from "react-icons/fa";
+import { MdOutlineAirlineSeatReclineExtra } from "react-icons/md";
+
 const CarCard = ({ car }) => {
   return (
     <div className="bg-white shadow-md rounded-lg overflow-hidden flex flex-col p-4">
@@ -8,10 +13,29 @@ const CarCard = ({ car }) => {
       />
       <div className="flex flex-col bg-white">
         <h2 className="bg-white text-xl font-semibold mb-2">{car.model}</h2>
-        <p className="bg-white text-gray-600 mb-1">{car.mark}</p>
-        <p className="bg-white text-gray-600 mb-1">
-          Transmission: {car.transmission}
-        </p>
+        <div className="mt-3 font-semibold flex flex-row items-center justify-start gap-7 md:gap-24 bg-white">
+          <div className="flex flex-col bg-white gap-3">
+            <div className="flex flex-row items-center gap-2 bg-white">
+              <BsFillFuelPumpFill className="text-blue-500 bg-white" />
+              <p className="bg-white text-gray-600">{car.mark}</p>
+            </div>
+            <div className="bg-white text-gray-600 flex flex-row items-center gap-2 mt-2">
+              <GiGearStickPattern className="bg-white text-blue-500" />
+              <p className="bg-white">{car.transmission}</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col bg-white gap-3">
+            <div className="flex flex-row items-center gap-2 bg-white">
+              <FaCalendarAlt className="text-blue-500 bg-white" />
+              <p className="bg-white text-gray-600">{car.releaseDate}</p>
+            </div>
+            <div className="bg-white text-gray-600 flex flex-row items-center gap-2 mt-2">
+              <MdOutlineAirlineSeatReclineExtra className="bg-white text-blue-500" />
+              <p className="bg-white">{car.seats}</p>
+            </div>
+          </div>
+        </div>
 
         <div className="bg-white flex flex-col sm:flex-row sm:item-center justify-between items-start mt-5 gap-3">
           <p className="bg-blue-500 text-white font-bold rounded-lg px-3 py-2 w-max">
