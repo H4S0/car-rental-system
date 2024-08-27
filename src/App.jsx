26fe68388ef0eaa769db947982ff235b8424d416
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-27 15:04:42 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,27 @@ import About from "./sections/About";
 import SignUp from "./sections/SignUp";
 import Navbar from "./components/Navbar";
 import Login from "./sections/Login";
-import { supabase } from "./services/supabase";
+import HowWorks from "./sections/HowWorks";
+import WhyChoose from "./sections/WhyChoose";
+import Contact from "./sections/Contact";
 
 function App() {
-  const [isLogged, setIsLogged] = useState(false);
-
-  useEffect(() => {
-    // Check session on app load to see if user is already logged in
-    const checkSession = async () => {
-      const session = await supabase.auth.getSession();
-      if (session.data?.session) {
-        setIsLogged(true);
-      } else {
-        setIsLogged(false);
-      }
-    };
-    checkSession();
-  }, []);
-
   return (
-    <div>
-      {/* Pass isLogged and setIsLogged to Navbar */}
-      <Navbar isLogged={isLogged} setIsLogged={setIsLogged} />
+    <div className="max-w-7xl mx-auto p-4">
+      <Navbar />
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/cars" element={<Cars />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login setIsLogged={setIsLogged} />} />
-      </Routes>
+      <div className="">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cars" element={<Cars />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/howworks" element={<HowWorks />} />
+          <Route path="/whychoose" element={<WhyChoose />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
     </div>
   );
 }
