5820e77f17f1e377bcce7bff1d49920010787c9f
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-27 12:56:54 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "scale-up": {
+          "0%": { transform: "scale(0.8)", opacity: "0" },
+          "100%": { transform: "scale(1)", opacity: "1" },
+        },
+      },
+      animation: {
+        "scale-up": "scale-up 0.3s ease-out",
+      },
+    },
   },
   plugins: [],
 };
