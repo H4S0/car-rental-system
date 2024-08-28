f6b81b0bb5d4f7861d466dc52c9e23bdb25c20e0
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-28 16:02:55 +0200

    Update OpenModal.jsx

--- src/components/OpenModal.jsx
+++ src/components/OpenModal.jsx
@@ -1,9 +1,12 @@
 import { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext"; 
+import { AuthContext } from "../contexts/AuthContext"; // Adjust the path as needed
+import { Link } from "react-router-dom";
+import RentForm from "./RentForm";
 
 const OpenModal = ({ closeModal }) => {
   const { isLogged } = useContext(AuthContext);
 
+  // Add a check to avoid rendering if context is not available
   if (typeof isLogged === "undefined") {
     return null;
   }
@@ -12,14 +15,24 @@ const OpenModal = ({ closeModal }) => {
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 backdrop-blur-sm">
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full animate-scale-up">
         <p className="text-gray-600 mb-4">
-          {isLogged ? <p>Form</p> : <p>Please login first</p>}
+          {isLogged ? <RentForm /> : <p>Please login first</p>}
         </p>
-        <button
-          onClick={closeModal}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg px-4 py-2 transition-all duration-300"
-        >
-          Close
-        </button>
+        <div className="flex justify-between items-center bg-white">
+          <button
+            onClick={closeModal}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg px-4 py-2 transition-all duration-300"
+          >
+            Close
+          </button>
+          {!isLogged && (
+            <Link
+              to="/login"
+              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg px-4 py-2 transition-all duration-300"
+            >
+              Login
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
