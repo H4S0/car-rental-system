de124ddb746d24e1f0a26ae720eef9a7acc91904
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-29 13:57:47 +0200

    Update CartContext.jsx

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const CartContext = createContext();
 
@@ -7,13 +7,25 @@ export const useCart = () => useContext(CartContext);
 export const CartProvider = ({ children }) => {
   const [rentedCars, setRentedCars] = useState([]);
 
+  useEffect(() => {
+    const storedCars = localStorage.getItem("rentedCars");
+    if (storedCars) {
+      setRentedCars(JSON.parse(storedCars));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("rentedCars", JSON.stringify(rentedCars));
+  }, [rentedCars]);
+
   const addCarToCart = (car) => {
     if (rentedCars.length < 2) {
       setRentedCars((prevCars) => [...prevCars, car]);
     } else {
-      alert("You cannot rent more that two cars");
+      alert("You cannot rent more than two cars");
     }
   };
+
   return (
     <CartContext.Provider value={{ rentedCars, addCarToCart }}>
       {children}
