19110241dd221ad13a98f29951cedac1759c6a22
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-30 10:55:14 +0200

    Update Login.jsx

--- src/sections/Login.jsx
+++ src/sections/Login.jsx
@@ -44,28 +44,44 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit" disabled={isLoading}>
-          {isLoading ? "Logging in..." : "Login"}
-        </button>
-      </form>
-      {error && <p style={{ color: "red" }}>{error}</p>}
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="text-center text-3xl font-bold text-blue-500 mb-6">
+        Login
+      </h2>
+      <div className="flex flex-col lg:flex-row gap-10 items-center p-8 rounded-lg mt-11 shadow-md">
+        <form
+          onSubmit={handleLogin}
+          className="flex flex-col w-full lg:w-1/2 space-y-4"
+        >
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="p-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+          />
+          <button
+            type="submit"
+            disabled={isLoading}
+            className="bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600 transition duration-300"
+          >
+            {isLoading ? "Logging in..." : "Login"}
+          </button>
+        </form>
+        <div className="mt-6 lg:mt-0 lg:ml-6 flex justify-center items-center rounded-full p-4 lg:p-6">
+          <img src="/src/assets/pngegg (1).png" alt="Audi A7" />
+        </div>
+      </div>
+      {error && <p className="text-red-500 mt-4">{error}</p>}
     </div>
   );
 };
