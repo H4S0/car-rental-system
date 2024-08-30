da2394cc379aafe628a1803f5ad4dd878dd40a53
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-08-30 09:34:15 +0200

    Update CartContext.jsx

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
-import { supabase } from "../services/supabase"; // Adjust the path as necessary
+import { supabase } from "../services/supabase"; 
 
 const CartContext = createContext();
 
