cec7c58f8ea111e46253c44f03b3f1ae45b1c8c5
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-09-01 09:55:33 +0200

    Update OpenModal.jsx

--- src/components/OpenModal.jsx
+++ src/components/OpenModal.jsx
@@ -9,20 +9,32 @@ const OpenModal = ({ closeModal, car }) => {
   const { addCarToCart } = useCart();
   const { isLogged, user } = useContext(AuthContext);
   const [isProcessing, setIsProcessing] = useState(false);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [city, setCity] = useState("");
   const navigate = useNavigate();
 
   const handleRentCar = async () => {
     setIsProcessing(true);
     try {
-      await addCarToCart(user.id, car.id); // Add car to cart
+      await addCarToCart(user.id, car.id, startDate, endDate, city); // Add city to cart
 
       closeModal();
       navigate("/rentedcars");
     } catch (error) {
-      console.error("Error during payment process:", error.message);
+      console.error("Error during rental process:", error.message);
     } finally {
-      setIsProcessing(false); // End processing
+      setIsProcessing(false);
     }
+  };
+
+  const handleDatesChange = (start, end) => {
+    setStartDate(start);
+    setEndDate(end);
+  };
+
+  const handleCityChange = (city) => {
+    setCity(city);
   };
 
   if (typeof isLogged === "undefined") {
@@ -32,9 +44,15 @@ const OpenModal = ({ closeModal, car }) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 backdrop-blur-sm">
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full animate-scale-up">
-        <p className="text-gray-600 mb-4">
-          {isLogged ? <RentForm /> : <p>Please login first</p>}
-        </p>
+        {isLogged ? (
+          <RentForm
+            car_id={car.id}
+            onDatesChange={handleDatesChange}
+            onCityChange={handleCityChange}
+          />
+        ) : (
+          <p>Please login first</p>
+        )}
         <div className="flex justify-between items-center bg-white">
           <button
             onClick={closeModal}
@@ -53,7 +71,7 @@ const OpenModal = ({ closeModal, car }) => {
             <button
               onClick={handleRentCar}
               className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md transition duration-300"
-              disabled={isProcessing} // Disable button while processing
+              disabled={isProcessing || !startDate || !endDate || !city}
             >
               {isProcessing ? "Processing..." : "Proceed to Payment"}
             </button>
